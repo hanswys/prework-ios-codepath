7e98adb55b3007c9deffa335bc0b73661e8d1a4e
hanswys <123027447+hanswys@users.noreply.github.com>
2025-03-24 16:32:41 -0500

    done with prework

---
+++ IOS101-Prework/README.md
@@ -0,0 +1,16 @@
+- Improve the UI by using different fonts/colors/background
+- Feel free to play around with other elements, such as image views, textfields, etc.
+- Implement a way to change the color of the text as well
+
+APP BRAINSTORMING
+1. Amazon 
+    - Efficient cart to shipping system
+    - Friendly UI 
+2. Maps 
+    - 3D modelling of locations 
+    - Closer view of roads providing clearer understanding of which lanes to turn
+3. American Express
+    - Convienient view of multiple accounts 
+    - Efficient use of home screen UI showing balance, payment and transaction all in one screen
+
+
--- IOS101-Prework/ViewController.swift
+++ IOS101-Prework/ViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeBackgroundColor: UIButton!
+    
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+            view.backgroundColor = randomColor
+        }
+    
+    
+    func changeColor() -> UIColor{
+
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
